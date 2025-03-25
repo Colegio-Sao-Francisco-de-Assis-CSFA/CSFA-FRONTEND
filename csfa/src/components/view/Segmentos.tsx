902073f9d9567265d761a8a56f8881d80c546666
Cloves-CSFA <cvr.neto20@gmail.com>
2025-03-25 14:19:00 -0300

    feat: asegment bg color yellow

--- csfa/src/components/view/Segmentos.tsx
+++ csfa/src/components/view/Segmentos.tsx
@@ -196,7 +196,7 @@ const EducationalSegments: React.FC = () => {
       frontDescription: "6º ao 9º ano do Fundamental",
       backDescription: "Aprofundamento dos conhecimentos através de abordagem multidisciplinar e desenvolvimento do pensamento crítico.",
       color: "bg-purple-500",
-      hoverColor: "bg-purple-600",
+      hoverColor: "bg-yellow-600",
       textColor: "text-white",
       icon: <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
